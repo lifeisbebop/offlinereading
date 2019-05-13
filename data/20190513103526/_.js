451c11e00c6693135e820a245a72475cb775599f
lifeisbebop <lifeisbebop@gmail.com>
2019-05-13 10:36:19 -0400

    2019/05/13 update #2

---
+++ data/20190513103526/_.js
@@ -0,0 +1,10 @@
+/**/ facebookCallback({
+   "og_object": {
+      "id": "2019964961455828",
+      "description": "The United States could find itself in not one but three nuclear crises in the next 12 months: one with North Korea, one with Russia, and one with Iran.",
+      "title": "The Year of Living Dangerously With Nuclear Weapons",
+      "type": "article",
+      "updated_time": "2019-05-11T00:01:02+0000"
+   },
+   "id": "https://www.foreignaffairs.com/articles/north-korea/2019-01-11/year-living-dangerously-nuclear-weapons?utm_medium=social"
+});
